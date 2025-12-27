8b44a603a7f9aaed905a5bc593e241e43bc99ac3
NghiaTran3124 <tykul258@gmail.com>
2025-12-27 22:11:07 +0700

    them error handle

--- src/js/parseQuery.js
+++ src/js/parseQuery.js
@@ -3,15 +3,36 @@ import MovieRankingLexer from "../parse/MovieRankingLexer.js";
 import MovieRankingParser from "../parse/MovieRankingParser.js";
 import ASTBuilder from "../ast/ASTBuilder.js";
 
+class ThrowingErrorListener extends antlr4.error.ErrorListener {
+  syntaxError(recognizer, offendingSymbol, line, column, msg, e) {
+    throw new Error(`Syntax error at line ${line}:${column} - ${msg}`);
+  }
+}
+
 export function parseQuery(input) {
   const chars = new antlr4.InputStream(input);
   const lexer = new MovieRankingLexer(chars);
+
+  lexer.removeErrorListeners();
+  lexer.addErrorListener(new ThrowingErrorListener());
+
   const tokens = new antlr4.CommonTokenStream(lexer);
   const parser = new MovieRankingParser(tokens);
 
+  parser.removeErrorListeners();
+  parser.addErrorListener(new ThrowingErrorListener());
   parser.buildParseTrees = true;
-  const tree = parser.query();
 
-  const visitor = new ASTBuilder();
-  return visitor.visit(tree);
+  try {
+    const tree = parser.query();
+
+    if (parser._input.LA(1) !== antlr4.Token.EOF) {
+      throw new Error("Extraneous input detected after valid query.");
+    }
+
+    const visitor = new ASTBuilder();
+    return visitor.visit(tree);
+  } catch (e) {
+    throw e;
+  }
 }
